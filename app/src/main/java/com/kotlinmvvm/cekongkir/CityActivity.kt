fd5838d93dd6b344f32a600267ae91fe746c25a9
nando <muhammad.rezki.ananda@gmail.com>
2021-04-06 15:22:38 +0700

    Add Activty, Fragment and Adapter Layout

---
+++ app/src/main/java/com/kotlinmvvm/cekongkir/CityActivity.kt
@@ -0,0 +1,11 @@
+package com.kotlinmvvm.cekongkir
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CityActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_city)
+    }
+}
