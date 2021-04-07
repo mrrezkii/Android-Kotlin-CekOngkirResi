65d007c271c4428376514467ce3da82ed2b29d79
nando <muhammad.rezki.ananda@gmail.com>
2021-04-07 11:01:58 +0700

    Get Layout from Lazday Indonesia

--- app/src/main/java/com/kotlinmvvm/cekongkir/ui/subdistrict/SubdistrictFragment.kt
+++ app/src/main/java/com/kotlinmvvm/cekongkir/ui/subdistrict/SubdistrictFragment.kt
@@ -17,7 +17,7 @@ private const val ARG_PARAM2 = "param2"
  * Use the [subdistrict.newInstance] factory method to
  * create an instance of this fragment.
  */
-class subdistrict : Fragment() {
+class SubdistrictFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -50,11 +50,11 @@ class subdistrict : Fragment() {
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            subdistrict().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+                SubdistrictFragment().apply {
+                    arguments = Bundle().apply {
+                        putString(ARG_PARAM1, param1)
+                        putString(ARG_PARAM2, param2)
+                    }
                 }
-            }
     }
 }
