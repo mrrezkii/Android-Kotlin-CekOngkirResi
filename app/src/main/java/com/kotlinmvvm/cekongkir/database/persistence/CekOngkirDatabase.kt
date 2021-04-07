9eff8f4ff94bc454776dde35ce2c2199a8b10d8d
nando <muhammad.rezki.ananda@gmail.com>
2021-04-07 11:07:05 +0700

    Get Database, Network, and Utils from Lazday Indonesia

--- app/src/main/java/com/kotlinmvvm/cekongkir/database/persistence/CekOngkirDatabase.kt
+++ app/src/main/java/com/kotlinmvvm/cekongkir/database/persistence/CekOngkirDatabase.kt
@@ -1,3 +1,24 @@
 package com.kotlinmvvm.cekongkir.database.persistence
 
-class CekOngkirDatabase
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class CekOngkirDatabase : RoomDatabase() {
+
+    companion object {
+        @Volatile
+        private var instance: CekOngkirDatabase? = null
+        private val LOCK = Any()
+
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: buildDatabase(context).also { instance = it }
+        }
+
+        private fun buildDatabase(context: Context) =
+                Room.databaseBuilder(context.applicationContext,
+                        CekOngkirDatabase::class.java, "CekOngkirLazday.db")
+                        .allowMainThreadQueries()
+                        .build()
+    }
+}
