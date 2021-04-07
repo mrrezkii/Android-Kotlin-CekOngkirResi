38801f1d43636713557fd9b31d7c243bec334098
nando <muhammad.rezki.ananda@gmail.com>
2021-04-07 16:29:24 +0700

    NavigationComponent (Android Jetpack)

--- app/src/main/java/com/kotlinmvvm/cekongkir/ui/city/CityFragment.kt
+++ app/src/main/java/com/kotlinmvvm/cekongkir/ui/city/CityFragment.kt
@@ -5,12 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.kotlinmvvm.cekongkir.R
+import com.kotlinmvvm.cekongkir.databinding.FragmentCityBinding
 
 class CityFragment : Fragment() {
 
+    private lateinit var binding: FragmentCityBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_city, container, false)
+        binding = FragmentCityBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.container.setOnClickListener {
+            findNavController().navigate(R.id.action_cityFragment_to_subdistrictFragment)
+        }
     }
 }
--- app/src/main/java/com/kotlinmvvm/cekongkir/ui/cost/CostFragment.kt
+++ app/src/main/java/com/kotlinmvvm/cekongkir/ui/cost/CostFragment.kt
@@ -1,15 +1,30 @@
 package com.kotlinmvvm.cekongkir.ui.cost
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.kotlinmvvm.cekongkir.R
+import com.kotlinmvvm.cekongkir.databinding.FragmentCostBinding
+import com.kotlinmvvm.cekongkir.ui.city.CityActivity
+
 class CostFragment : Fragment() {
+
+    private lateinit var binding: FragmentCostBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_cost, container, false)
+        binding = FragmentCostBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.editOrigin.setOnClickListener {
+            startActivity(Intent(context, CityActivity::class.java))
+        }
+    }
+
+
 }
