d011184ec94513d22a6468608798e0d36dcfaf39
nando <muhammad.rezki.ananda@gmail.com>
2021-04-07 15:59:36 +0700

    ViewBinding (Android Jetpack)

--- app/src/main/java/com/kotlinmvvm/cekongkir/ui/home/HomeActivity.kt
+++ app/src/main/java/com/kotlinmvvm/cekongkir/ui/home/HomeActivity.kt
@@ -2,27 +2,24 @@ package com.kotlinmvvm.cekongkir.ui.home
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.viewpager2.widget.ViewPager2
-import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
-import com.kotlinmvvm.cekongkir.R
+import com.kotlinmvvm.cekongkir.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
 
-    val tabLayout by lazy { findViewById<TabLayout>(R.id.tab_layout) }
-    val viewPager by lazy { findViewById<ViewPager2>(R.id.view_pager) }
+    private val binding by lazy { ActivityHomeBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(binding.root)
         setupTab()
     }
 
     private fun setupTab() {
         val tabTitles = arrayListOf("Cek Ongkir", "Cek Resi")
         val tablAdapter = HomeTabAdapter(supportFragmentManager, lifecycle)
-        viewPager.adapter = tablAdapter
-        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+        binding.viewPager.adapter = tablAdapter
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
             tab.text = tabTitles[position]
         }.attach()
     }
